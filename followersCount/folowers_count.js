8947ef45a53b2b3d7db5e0deced88706f60b287b
CSouth33 <christian.southards@gmail.com>
2026-08-06 18:12:27 -0400

    followersCountt task

---
+++ followersCount/folowers_count.js
@@ -0,0 +1,19 @@
+let count = 0; // Initialize count to 0
+
+function increaseCount() {
+    count++; //Increment count by 1
+    displayCount(); // Display the count
+    checkCountValue(); //Check the count value and display messages
+}
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count; // Display the count
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram profile gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+        alert("Your Instagram profile gained 20 followers! Keep it up!");
+    }
+}
