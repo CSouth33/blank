380a7ecedee9ae3a6351b8f43511f0e31f97a140
CSouth33 <christian.southards@gmail.com>
2026-08-13 19:39:31 -0400

    restaurant menu prac task

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,8 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+
+const mainCourseMenu = ['Steak - $32', 'Pasta - $17', 'Burger - $18 ', 'Salmon - $24'];
+
+const dessertMenu = ['Cake - $7', 'Ice Cream - $6', 'Pudding - $5 ', 'Fruit Salad - $6'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
